e3884fee90fc5669265788af485f774393437ca4
Jim McAndrew <jim@loc8.us>
2014-03-22 14:56:51 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+= OSM Merge
+The main purpose of this project is to allow users to merge the content on one OpenStreetMap project into another.
+Some examples would be:
+ * Merging Public Domain Data from the [USGS National Map Corps](http://navigator.er.usgs.gov) system into OpenStreetMap
+ * Merging data from compatible datasets that do not conflate well
---
+++ gruntfile.js
@@ -0,0 +1,78 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    browserify: {
+      options: {
+        banner: '/*! Browserify: <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+        }
+      }
+    },
+    clean: {
+      dist: {
+        src: ['dist/**/*']
+      }
+    },
+    copy: {
+      dist: {
+        src: 'src/index.html',
+        dest: 'dist/index.html'
+      }
+    },
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: ['src/**/*.js'],
+        dest: 'dist/<%= pkg.name %>.js'
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          'dist/<%= pkg.name %>.min.js': ['dist/*.js']
+        }
+      }
+    },
+    qunit: {
+      files: ['test/**/*.html']
+    },
+    jshint: {
+      files: ['Gruntfile.js', 'src/**/*.js', 'test/**/*.js'],
+      options: {
+        // options here to override JSHint defaults
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true,
+          document: true
+        }
+      }
+    },
+    watch: {
+      files: ['<%= jshint.files %>'],
+      tasks: ['jshint', 'qunit']
+    }
+  });
+
+  //grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  //grunt.loadNpmTasks('grunt-contrib-qunit');
+  //grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-browserify');
+
+  //grunt.registerTask('test', ['jshint', 'qunit']);
+  grunt.registerTask('default', ['clean', 'jshint', 'concat', 'browserify', 'copy:dist']);
+
+};
