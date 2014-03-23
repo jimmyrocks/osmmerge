43b35f6a1f969fce8886ac8181bde77b60158c94
Jim McAndrew <jim@loc8.us>
2014-03-22 20:00:31 -0600

    Add grunt watch

--- gruntfile.js
+++ gruntfile.js
@@ -68,7 +68,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-clean');
   //grunt.loadNpmTasks('grunt-contrib-qunit');
-  //grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-browserify');
 
