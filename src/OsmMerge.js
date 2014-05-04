faf9216b9ad35c92deb77ed771b098e6f5c39029
Jim McAndrew <jim@loc8.us>
2014-05-04 17:13:00 -0600

    Add init and content directly

--- src/OsmMerge.js
+++ src/OsmMerge.js
@@ -2,3 +2,7 @@ window.L = require('leaflet/dist/leaflet-src');
 require('../src/plugins/Bing.js');
 require('leaflet-osm');
 require('leaflet-sidebar');
+window.L.osmMerge = {
+  init: require('../src/osmMerge/init.js'),
+  content: require('../src/osmMerge/content.js')
+};
