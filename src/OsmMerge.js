3ad997a4b39e60e629c2cf3c9b226984630d8e4a
Jim McAndrew <jim@loc8.us>
2014-05-04 11:32:30 -0600

    remove some libraries to get to the point with this app

--- src/OsmMerge.js
+++ src/OsmMerge.js
@@ -1,5 +1,4 @@
 window.L = require('leaflet/dist/leaflet-src');
 require('../src/plugins/Bing.js');
-require('leaflet-draw');
 require('leaflet-osm');
-require('leaflet-osm-edit');
+require('leaflet-sidebar');
