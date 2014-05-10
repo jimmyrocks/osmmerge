f8a633f6e5fe26a80bdd22d7ec8f4864d3dcdb6c
Jim McAndrew <jim@loc8.us>
2014-05-10 10:05:04 -0600

    Add new functions for buttons

--- src/osmMerge/init.js
+++ src/osmMerge/init.js
@@ -1,14 +1,40 @@
 var showIntro = function() {
-  $('#sidebar').empty();
-  $('.header').empty();
+  console.log('DON\'T USE THIS FUNCTION');
+  showPage('intro');
+    $('.header').empty();
+    $('.header')[0].appendChild(htmlWrap('h3',L.osmMerge.content.project.title + ': ' + L.osmMerge.content.project.tagline, 'title'));
+},
+drawButtons = function(buttonList, id) {
+  var button, buttonId, buttonObj,
+  buttonContainer = L.DomUtil.create('div', id);
+  for (buttonId=0; buttonId < buttonList.length; buttonId++) {
+    button = buttonList[buttonId];
+    buttonObj = L.DomUtil.create('button', 'btn btn-' + (button.style || 'primary'));
+    buttonObj.textContent = button.title;
+    buttonContainer.appendChild(buttonObj);
+  }
+  return buttonContainer;
+},
+showPage = function(page) {
+  var pageObj;
+  // Allow page name or page object to be passed in
+  if (typeof(page) === 'string') {
+    pageObj = L.osmMerge.content.sidebars[page];
+  } else {
+    pageObj = page;
+  }
+  var content = [
+    htmlWrap('h2', pageObj.header, 'sidebarHeader'),
+    htmlWrap('h4', pageObj.subheader, 'sidebarSubheader'),
+    htmlWrap('p', pageObj.content, 'sidebarContent'),
+    drawButtons(pageObj.buttons, 'sidebarButtons')
+  ];
 
-  $('#sidebar')[0].appendChild(htmlWrap('h2', L.osmMerge.content.sidebars.intro.header));
-  $('#sidebar')[0].appendChild(htmlWrap('p', L.osmMerge.content.sidebars.intro.subheader));
-  $('#sidebar')[0].appendChild(L.osmMerge.content.sidebars.intro.content);
-
-  $('.header')[0].appendChild(htmlWrap('h3',L.osmMerge.content.project.title + ': ' + L.osmMerge.content.project.tagline, 'title'));
-
-  L.osmMerge.controls.getByName('sidebar')[0].show();
+$('#sidebar').empty();
+for (var i=0; i<content.length; i++) {
+  $('#sidebar')[0].appendChild(content[i]);
+}
+L.osmMerge.controls.getByName('sidebar')[0].show();
 },
 htmlWrap = function(tag, content, id) {
   var temp = L.DomUtil.create(tag, id);
@@ -72,6 +98,21 @@ module.exports = function(mapDiv, layers, defaultLayer) {
     return L.osmMerge.controls.filter(function(a) {
       return a && a.options && a.options.name === name;
     });
+  };
+  L.osmMerge.utils = {
+    fandleBars: function(text, replaceValues) {
+      // This is my quick and dirty version of handlebars
+      var re = function(name) {
+        return new RegExp('{{' + name + '}}', 'g');
+      },
+        replaceValue;
+      for (replaceValue in replaceValues) {
+        if (text.search(re(replaceValue)) >= 0) {
+          text = text.replace(re(replaceValue), replaceValues[replaceValue]);
+        }
+      }
+      return text;
+    }
   };
 
   // Add the controls
