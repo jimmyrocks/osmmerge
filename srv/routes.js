bc2e0d904e85a5096e01d90db7fd365c0d8e9f70
Jim McAndrew <jim@loc8.us>
2014-05-12 19:34:11 -0600

    add dist path

--- srv/routes.js
+++ srv/routes.js
@@ -1,14 +1,16 @@
 var send = require('koa-send'),
   queries = require('./sql.json');
+  distDir = '/' + __dirname.split('/').slice(1,-1).join('/') + '/dist',
 module.exports = {
   '/': function * root() {
-    yield send(this, '/home/jim/dev/osmmerge/dist/index.html');
+    console.log(distDir);
+    yield send(this, distDir + '/index.html');
   },
   '/:file.:suffix': function * getFile(file, suffix) {
-    yield send(this, '/home/jim/dev/osmmerge/dist/' + file + '.' + suffix);
+    yield send(this, distDir + '/' + file + '.' + suffix);
   },
   '/images/:file.png': function * getFile(file) {
-    yield send(this, '/home/jim/dev/osmmerge/dist/images/' + file + '.png');
+    yield send(this, distDir + '/' + file + '.png');
   },
   '/get/new': function * getNewPoint(format) {
     var result = yield this.pg.db.client.query_(queries.select.randomPoint.join(''));
