26b476a2b8a9dc6894cf9e4afed0b1b653f33497
Jim McAndrew <jim@loc8.us>
2014-05-12 19:37:05 -0600

    Fix images route

--- srv/routes.js
+++ srv/routes.js
@@ -10,7 +10,7 @@ module.exports = {
     yield send(this, distDir + '/' + file + '.' + suffix);
   },
   '/images/:file.png': function * getFile(file) {
-    yield send(this, distDir + '/' + file + '.png');
+    yield send(this, distDir + '/images/' + file + '.png');
   },
   '/get/new': function * getNewPoint(format) {
     var result = yield this.pg.db.client.query_(queries.select.randomPoint.join(''));
